9361f6de4b6512318d585033589854ca096c355c
Sadhvi12 <bezawadasadhvi23@gmail.com>
2020-12-10 15:04:52 +0530

    first commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Todo from "./Todo";
+import { List } from "@material-ui/core";
+
+function TodoList({todos,toggleComplete, removeTodo}) {
+    return(
+        <List>
+        <ul>
+            {todos.map(todo => (
+            <Todo 
+                key={todo.id} 
+                todo={todo} 
+                toggleComplete={toggleComplete}
+                removeTodo={removeTodo}
+                />
+            ))}
+        </ul>
+        </List>
+    );
+}
+
+export default TodoList;
